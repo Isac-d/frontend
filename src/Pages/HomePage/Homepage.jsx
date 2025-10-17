525566857a0466ed9fdc367a62d96f8aae84c091
sesar <54498315+sesar2@users.noreply.github.com>
2025-10-17 09:34:31 +0200

    room card

--- src/Pages/HomePage/Homepage.jsx
+++ src/Pages/HomePage/Homepage.jsx
@@ -1,12 +1,30 @@
 import { Link } from "react-router-dom";
 import "./homepage.css";
 import RoomCard from "../../components/RoomCard/RoomCard";
+import { FaCalendarDay } from "react-icons/fa";
+
 const Homepage = () => {
-  const rooms = [
-    { guestCount: 2, price: 200, roomTypes: "Single/Double", roomImg: 'src/assets/linus-mimietz-p3UWyaujtQo-unsplash.jpg' },
-    { guestCount: 3, price: 350, roomTypes: "Double", roomImg: 'src/assets/vojtech-bruzek-Yrxr3bsPdS0-unsplash.jpg' },
-    { guestCount: 6, price: 750, roomTypes: "Suite", roomImg:  'src/assets/reisetopia-aI6Su7Mu9Ro-unsplash.jpg'},
-  ];
+const rooms = [
+  {
+    guestCount: 2,
+    price: 200,
+    roomTypes: "Deluxe Double Room",
+    roomImg: "src/assets/linus-mimietz-p3UWyaujtQo-unsplash.jpg",
+  },
+  {
+    guestCount: 3,
+    price: 350,
+    roomTypes: "Executive Family Room",
+    roomImg: "src/assets/vojtech-bruzek-Yrxr3bsPdS0-unsplash.jpg",
+  },
+  {
+    guestCount: 6,
+    price: 750,
+    roomTypes: "Presidential Suite",
+    roomImg: "src/assets/reisetopia-aI6Su7Mu9Ro-unsplash.jpg",
+  },
+];
+
 
   return (
     <>
@@ -20,8 +38,8 @@ const Homepage = () => {
             Where elegance meets exceptional service.
           </h3>
           <div className="hero-button-container">
-            <Link to={"/"}>
-              <button className="booking-link">Book your stay</button>
+            <Link style={{textDecoration: 'none'}} to={"/"}>
+              <button className="booking-link"><FaCalendarDay/>Book your stay</button>
             </Link>
             <button className="explore-rooms-btn">Explore our rooms</button>
           </div>
@@ -48,6 +66,10 @@ const Homepage = () => {
           ))}
         </div>
       </secton>
+              <div className="section-header">
+
+        </div>
+        <footer></footer>
     </>
   );
 };
--- src/components/RoomCard/RoomCard.jsx
+++ src/components/RoomCard/RoomCard.jsx
@@ -1,15 +1,24 @@
+import './roomcard.css'
+import { FaWifi } from "react-icons/fa";
+import { MdPeopleAlt } from "react-icons/md";
+import { FaBreadSlice } from "react-icons/fa";
+
+
 const RoomCard = ({ guestCount, price, roomType, roomImg,  }) => {
   return (
     <div className="room-card">
         <div className="room-img-container">
-        <img src={roomImg} alt="" />
-        <div className="price-badge">{price}</div>
+        <img className='room-img' src={roomImg} alt="" />
+        <div className="price-badge">From {price}€</div>
         </div>
         <div className="room-card-info">
+            <div className="top-card">
             <div className="room-title">{roomType}</div>
-            <div className="guest-wifi-container">
-               <div className="guests">{guestCount} Guests</div>
-               <div className="wifi">Free wifi</div> 
+            <div className="room-info-container">
+               <div className="room-info"><MdPeopleAlt style={{color: '#dd9804', fontSize: '20px'}}/> {guestCount} Guests</div>
+               <div className="room-info"><FaWifi style={{color: '#dd9804', fontSize: '20px'}}/> Free wifi</div> 
+               <div className="room-info"><FaBreadSlice style={{color: '#dd9804', fontSize: '20px'}}/> Breakfast included</div>
+            </div>
             </div>
             <button className="view-details-button">View details</button>
         </div>
